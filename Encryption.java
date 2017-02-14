3be0a36792b7375d47ffbd3cf81f85447eae8041
premkumar97 <premkumar97@users.noreply.github.com>
2017-02-14 15:20:35 +0530

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -1,12 +1,14 @@
 package com.prem.s;
 
 import java.security.*;
+import java.util.Scanner;
+
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 
 import sun.misc.*;
 
-public class Encryption {
+public class AESencrp {
     
      private static final String ALGO = "AES";
     private static final byte[] keyValue = 
@@ -25,9 +27,14 @@ private static Key generateKey() throws Exception {
         Key key = new SecretKeySpec(keyValue, ALGO);
         return key;
 }
- public static void main(String args[]) throws Exception{
-    	String a =encrypt("abcd");
+    public static void main(String args[]) throws Exception{
+    	Scanner scanner=new Scanner(System.in);
+    	System.out.println("Input 1:");
+    	String s1=scanner.next();
+    	System.out.println("\nInput 2:");
+    	String s2=scanner.next();
+    	String a =encrypt(s1.concat(s2));
     	System.out.println(a);
-    
+    	
     }
 }
